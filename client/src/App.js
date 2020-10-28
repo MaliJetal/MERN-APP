3390f5666771952d6e8ad0106e22e79c94f55195
Jetal <malijetal1234@gmail.com>
2020-10-29 02:22:48 +0530

    Setup Login/Register Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>My React App</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path = '/' component={Landing} />
+        <div className="container">
+          <Route exact path = '/Login' component = {Login} />
+          <Route exact path = '/Register' component = {Register} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
