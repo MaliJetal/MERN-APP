9328ccfb69d6385338a37a469d313606b799a224
Jetal <malijetal1234@gmail.com>
2020-11-08 23:34:24 +0530

    Setup the Redux States and props

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
@@ -9,20 +11,24 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path = '/' component={Landing} />
-        <div className="container">
-          <Route exact path = '/Login' component = {Login} />
-          <Route exact path = '/Register' component = {Register} />
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <div className="container">
+              <Route exact path='/Login' component={Login} />
+              <Route exact path='/Register' component={Register} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -30,15 +32,17 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     }
-    axios.post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors : err.response.data}));
+
+    this.props.registerUser(newUser);
   }
 
   render() {
-    const {errors} = this.state;
+    const { errors } = this.state;
+
+    const { user } = this.props.auth;
     return (
       <div className="register">
+        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -47,28 +51,28 @@ class Register extends Component {
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input type="text" className={classnames('form-control form-control-lg', {
-                    'is-invalid' : errors.name
+                    'is-invalid': errors.name
                   })} placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
-                  {errors.name && (<div className = "invalid-feedback">{errors.name}</div>)}
+                  {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                 </div>
                 <div className="form-group">
                   <input type="email" className={classnames('form-control form-control-lg', {
-                    'is-invalid' : errors.email
+                    'is-invalid': errors.email
                   })} placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
-                  {errors.email && (<div className = "invalid-feedback">{errors.email}</div>)}
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                   <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
                 <div className="form-group">
                   <input type="password" className={classnames('form-control form-control-lg', {
-                    'is-invalid' : errors.password
+                    'is-invalid': errors.password
                   })} placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
-                  {errors.password && (<div className = "invalid-feedback">{errors.password}</div>)}
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <div className="form-group">
                   <input type="password" className={classnames('form-control form-control-lg', {
-                    'is-invalid' : errors.password2
+                    'is-invalid': errors.password2
                   })} placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
-                  {errors.password2 && (<div className = "invalid-feedback">{errors.password2}</div>)}
+                  {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -80,4 +84,14 @@ class Register extends Component {
     )
   }
 }
-export default Register;
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStoreToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStoreToProps, { registerUser })(Register);
