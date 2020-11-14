ec2b1641136aa1807aaf0ecab72f1a016f6dc34b
Jetal <malijetal1234@gmail.com>
2020-11-15 02:48:57 +0530

    Redux Register/Login Action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -13,3 +13,24 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+//Login - Get User Token
+export const registerUser = (userData) => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //save to localstorage
+      const { token } = res.data;
+      //set token to ls
+      localStorage.setItem('jwtToken', token);
+      //Set token to Auth Header
+      setAuthToken(token);
+
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
