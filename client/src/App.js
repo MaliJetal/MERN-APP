2d7eaaf43e2e2ce842c2ae6035db197c39629ba8
Jetal <malijetal1234@gmail.com>
2020-11-16 05:14:11 +0530

    Sustaining login part

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,23 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
 
+import setAuthToken from '../utils/setAuthToken';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { setCurrentUser } from './actions/authActions';
 
+if (localStorage.jwtToken) {
+  //Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
