ac30b6bff9854d090c5fe975f56b9d35b9aec858
Jetal <malijetal1234@gmail.com>
2020-11-17 02:47:48 +0530

    set logout auth action

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { jwt_decode } from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
 
-import setAuthToken from '../utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+import setAuthToken from './utils/setAuthToken';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import { setCurrentUser } from './actions/authActions';
 
 if (localStorage.jwtToken) {
   //Set auth token header auth
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -47,3 +47,13 @@ export const setCurrentUser = (decoded) => {
     payload: decoded
   }
 }
+
+//logged out user
+export const logoutUser = () => dispatch => {
+  //Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  //Remove auth header for future request
+  setAuthToken(false);
+  //Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+}
