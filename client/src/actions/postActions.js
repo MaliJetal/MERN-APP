639e9f4bf872c8f3de810edfbfacaf86e281fba2
Jetal <malijetal1234@gmail.com>
2021-03-01 00:41:27 +0530

    Post Items-Like Unlike

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 import {
   ADD_POST,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST,
   GET_ERRORS
 } from './types';
 
+//Add Post
 export const addPost = postData => dispatch => {
   axios
     .post('./api/posts', postData)
@@ -21,3 +25,73 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+//get posts
+export const getPosts = () => dispatch => {
+  dispatch(post_loading());
+  axios
+    .get('./api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Add like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+//Remove like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
+
+//posts loading
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
+}
